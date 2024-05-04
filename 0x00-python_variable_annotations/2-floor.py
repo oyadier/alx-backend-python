1d075904678c758dd50310f5b7b4c8a62476ad6d
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-05-04 01:36:37 +0000

    Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import math
+'''find floor of a number'''
+
+
+def floor(n: float) -> float:
+    '''
+    floor any king of n value
+        Args:
+        n (number): a entere float number
+    Return the floor of a number
+    '''
+    return math.floor(n)
