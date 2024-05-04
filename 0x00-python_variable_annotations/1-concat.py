52194d9dde169278b246e2904a096fe31ea0acb5
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-05-04 01:25:33 +0000

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''Concatenate two strings'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''
+    concat: concate two parameters
+        Args:
+            str1(string)
+            str2(string)
+        Returns: a concatenated string
+    '''
+
+    return str1 + str2
