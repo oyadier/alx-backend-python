ba3503144714d1832e3c7a6b3ac267e6e56c8911
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-05-04 02:03:22 +0000

    Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''convert a parameter to string'''
+
+
+def to_str(n: float) -> str:
+    '''
+    to_str: a compute the string a value
+        Args:
+            n(number): either float or int value
+    Return: a string of the value
+    '''
+    return str(n)
