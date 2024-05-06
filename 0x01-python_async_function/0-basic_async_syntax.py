3d91635b1aafbedb3440fd7fe42ce9c50c46e285
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-05-06 21:51:46 +0000

    The basics of async

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
+'''The basics of aysnc'''
 import asyncio
 import random
-'''The basics of aysnc'''
 
 
 async def wait_random(max_delay=10):
-    '''Find random number between specified range
+    '''
+    Find random number between specified range
         Args:
-            max_delay(number): maximum number of random number
+            max_delay(integer): maximum number of random number
 
-        Return(number): the random number
+        Return(integer): the random number
     '''
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
