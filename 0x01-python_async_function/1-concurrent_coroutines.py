f545b200764e84cb9fe58890d298d89fa101b02a
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-05-06 22:51:31 +0000

    Let's execute multiple coroutines at the same time with async mandatory

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -6,6 +6,13 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
+    '''
+    wait_n: Run multiple corotine at a time
+        Args:
+            n (integer): number of a corotine should be run
+            max_delay (integer): maximun random number
+        Return(List[Float]): return bedback from all corotine
+    '''
 
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
     return [await task for task in asyncio.as_completed(tasks)]
