c69bb7992f5085ec7ad67f99e814763fa88f0211
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-05-07 22:01:52 +0000

    Async Generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+import asyncio
+'''Async Generator'''
+from random import uniform
+
+
+async def async_generator():
+    '''
+    async_generator: return random number using yeild
+        Return: yoild
+    '''
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield uniform(0, 10)
