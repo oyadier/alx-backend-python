dc3da775045fbe95635bb5b74c0477b73dbeb217
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-05-27 21:37:01 +0000

    Mock HTTP calls

--- 0x03-Unittests_and_integration_tests/0-main.py
+++ 0x03-Unittests_and_integration_tests/0-main.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
-
-nested = __import__('utils').access_nested_map
+import utils
 
 if __name__ == "__main__":
     
     nested_map={'oya':{'om':{'ro':'pat', 'is':'fat'}}}
     
     try:
-        print(nested(nested_map, ['oya','om', 'rob']))
+        print(utils.access_nested_map(nested_map, ['oya','om', 'rob']))
     except KeyError:
         print("Key Error, it could'nt be found")
+        
+    response = utils.get_json("http://example.com")
+    print(response)
--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,6 +3,8 @@
 import unittest
 from utils import access_nested_map
 from parameterized import parameterized
+from unittest.mock import MagicMock, patch
+from utils import get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -27,3 +29,21 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map_exception(self, input, path):
         with self.assertRaises(KeyError):
             access_nested_map(input, path)
+
+
+class TestGetJson(unittest.TestCase):
+
+    @patch('utils.requests.get')
+    @parameterized.expand(
+        [
+            ("http://example.com", {"payload": True}),
+            ("http://holberton.io", {"payload": False})
+        ])
+    def test_get_json(self, input, output, mock_requests):
+        response = MagicMock()
+        response.json.return_value = output
+        mock_requests.return_value = response
+
+        result = get_json(input)
+        mock_requests.assert_called_once_with(result)
+        self.assertEqual(result, output)
